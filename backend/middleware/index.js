da444007c31b153f747e1598d4226631f982cee9
Tuba Mahmood <samkap333@gmail.com>
2024-10-14 15:44:56 +0530

    Delete backend directory

--- backend/middleware/index.js
+++
@@ -1,9 +0,0 @@
-const cors = require('cors');
-const bodyParser = require('body-parser');
-
-const applyMiddleware = (server) => {
-    server.use(cors());
-    server.use(bodyParser.json());
-};
-
-module.exports = applyMiddleware;
