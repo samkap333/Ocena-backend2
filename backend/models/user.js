4a1baeded85e487d57686b7840723577d818afde
Tuba Mahmood <samkap333@gmail.com>
2025-02-07 13:07:35 +0530

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true,unique: false },
+    phoneNumber: { type: Number, required: true, unique: false },
     email: { type: String, required: true, unique: false },
-    message: { type: String, required: true}
-   
+    subject: { type: String, required: true},
+    message: { type: String, required: true},
+    createdAt: { type: Date, default: Date.now }
     
 });
 
